b6c31d4cbf407fbbd4fad4e12fe3c53eed65510d
Yeray <102992791+YerayAR89@users.noreply.github.com>
2022-12-23 09:25:51 +0100

    Add files via upload

---
+++ card-game/js/cardtypes.js
@@ -0,0 +1,58 @@
+export let cardtypes = [
+    {
+        name: 'Minor healing potion',
+        value: 3,
+        image: 'healingpotion',
+        type: 'heal'
+    },
+    {
+        name: 'Major healing potion',
+        value: 6,
+        image: 'healingpotion',
+        type: 'heal'
+    },
+    {
+        name: 'Shield',
+        value: 5,
+        image: 'shield',
+        type: 'armor'
+    },
+    {
+        name: 'Kobold',
+        value: 6,
+        image: 'kobold',
+        type: 'attack'
+    },
+    {
+        name: 'Troll',
+        value: 8,
+        image: 'troll',
+        type: 'attack'
+    },
+    {
+        name: 'Ogre',
+        value: 10,
+        image: 'ogre',
+        type: 'attack'
+    },
+    {
+        name: 'Death Knight',
+        value: 12,
+        image: 'deathknight',
+        type: 'attack'
+    },
+    {
+        name: 'Fire Drake',
+        value: 16,
+        image: 'firedrake',
+        type: 'attack'
+    },
+    {
+        name: 'Golden Dragon',
+        value: 20,
+        image: 'goldendragon',
+        type: 'attack'
+    },
+];
+
+export default cardtypes;
